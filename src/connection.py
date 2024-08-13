da2e6d508c1cbe3cd79c9c8c8efa302ce1a0d483
Saif <admin@saifuddin.uk>
2024-08-13 14:36:50 +0100

    merging stashed changes

--- src/connection.py
+++ src/connection.py
@@ -1,15 +1,40 @@
 from pg8000.native import Connection
-from dotenv import load_dotenv
+from pg8000.exceptions import DatabaseError
+# from dotenv import load_dotenv
 import os
+from src.utils import get_secret
 
-load_dotenv()
+# load_dotenv()
+
+# {
+#     'username': 'project_team_3',
+#     'password': '3oOhexQmO2Rafbf',
+#     'engine': 'postgres',
+#     'host': 'nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com',
+#     'port': '5432',
+#     'dbname': 'totesys'
+# }
+
+#if __name__ == "__main__":
 
 def connect_to_db():
-    return Connection(
-        user=os.getenv("Username"),
-        password=os.getenv("Password"),
-        database=os.getenv("Database"),
-        host=os.getenv("Hostname"),
-        port=int(os.getenv("Port"))
-    )
+    try:
+        credentials = get_secret()
+        return Connection(
+            user=credentials['username'],
+            password=credentials['password'],
+            database=credentials["dbname"],
+            host=credentials['host'],
+            port=int(credentials['port'])
+        )
     
+    except DatabaseError as e:
+        print(e)
+        raise e
+
+def test():
+    conn = connect_to_db()
+    query = '''SELECT * FROM payment
+limit 1;
+'''
+    return conn.run(query)
---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
+from datetime import date
+from decimal import Decimal
+
+def get_secret():
+    secret_name = "project-onyx/totesys-db-login"
+    region_name = "eu-west-2"
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+    secret = get_secret_value_response['SecretString']
+    result_dict = json.loads(secret)
+    return result_dict
+
+def format_response(columns, response):
+    formatted_response = []
+    for row in response:
+        extracted_from_response = {}
+        for i in range(len(columns)):
+            if type(row[i]) == type(date(2000,1,1)):
+                row[i] = row[i].strftime("%Y-%m-%d")
+            if type(row[i]) == type(Decimal('1.00')):
+                row[i] = float(row[i])
+            extracted_from_response[columns[i]] = row[i]
+        formatted_response.append(extracted_from_response)
+    return formatted_response
