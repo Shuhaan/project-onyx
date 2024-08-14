989afb261622bbb56e03999f562289eeb392b4f6
Saif <admin@saifuddin.uk>
2024-08-14 11:20:15 +0100

    testing extract

--- src/extract.py
+++ src/extract.py
@@ -10,22 +10,23 @@ import json
 # logger = logging.getLogger("OnyxLogger")
 # logger.setLevel(logging.INFO)
 
-
-
-extract_time = datetime.now()
-extract_year = extract_time.year
-extract_month = extract_time.month
-extract_day = extract_time.day
-extract_hour = extract_time.hour
-extract_minute = extract_time.minute
-
 most_recent_extract = ''
 
-def extract_from_db():
+def extract_from_db(bucket_name, s3_resource=boto3.resource('s3')):
+
+    bucket = s3_resource.Bucket(bucket_name)
+
+    extract_time = datetime.now()
+    extract_year = extract_time.year
+    extract_month = extract_time.month
+    extract_day = extract_time.day
+    extract_hour = extract_time.hour
+    extract_minute = extract_time.minute
+
     totesys_table_list = ['address', 'design', 'transaction', 'sales_order', 
                       'counterparty', 'payment', 'staff', 'purchase_order', 
                       'payment_type', 'currency', 'department' ]
-    s3_client = boto3.client('s3')
+    
     conn = connect_to_db()
 
     for table in totesys_table_list:
@@ -46,15 +47,15 @@ def extract_from_db():
                     
         most_recent_extract = s3_key
         
-        # response = s3_client.put_object(
-        #     Bucket='onyx_ingestion_bucket',
-        #     Key='s3_key',
-        #     Body=extracted_json,
-        # )
+        response = bucket.put_object(
+            Key='s3_key',
+            Body=extracted_json,
+        )
         
         print(s3_key)
     conn.close()
+    return extracted_json
         
-extract_from_db()
+# extract_from_db()
 
 
--- test/test_extract.py
+++ test/test_extract.py
@@ -3,3 +3,39 @@ from src.extract import extract_from_db
 from moto import mock_aws
 from unittest import mock
 import boto3
+from src.extract import extract_from_db
+from utilities.utils import create_s3_bucket, upload_to_s3, \
+    view_bucket_contents, credentials_storer, secret_retriever
+
+@pytest.fixture()
+def s3_resource():
+    with mock_aws():
+        yield boto3.resource('s3')
+
+
+@pytest.fixture()
+def secretsmanager_client():
+    with mock_aws():
+        yield boto3.client('secretsmanager')
+
+
+@pytest.mark.skip()
+def test_get_bucket_contents(s3_resource):
+    bucket = create_s3_bucket('test-bucket', s3_resource=s3_resource)
+    upload_to_s3('test.txt', 'test-bucket')
+    upload_to_s3('test2.txt', 'test-bucket')
+    assert view_bucket_contents(bucket) == ['test.txt', 'test2.txt']
+
+
+def test_extract(s3_resource):   
+    bucket = create_s3_bucket('test-bucket', s3_resource=s3_resource)
+    print(bucket)
+    assert extract_from_db('test-bucket', s3_resource=s3_resource) == view_bucket_contents(bucket)
+
+@pytest.mark.skip()
+def test_get_secret(secretsmanager_client):
+    credentials_storer('secret', 'bob', 'marley', secretsmanager_client)
+    assert secret_retriever('secret') == '''{
+    "username":"userId",
+    "password":"password"
+}'''
