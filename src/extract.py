e5f580e77011cabe0cab3b07ef65f954a1b569d6
Saif <admin@saifuddin.uk>
2024-08-14 16:59:52 +0100

    updated testing

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,6 @@
 from src.connection import connect_to_db
-from utilities.utils import format_response
+from utilities.utils import format_response, log_message
+from botocore.exceptions import ClientError
 import logging
 import boto3
 from datetime import datetime
@@ -14,57 +15,61 @@ most_recent_extract = ""
 
 
 # def extract_from_db(bucket_name, s3_resource=boto3.resource('s3')):
-def extract_from_db_write_to_s3(bucket):
+def extract_from_db_write_to_s3(s3_client, bucket):
     # bucket = s3_resource.Bucket(bucket_name)
+    try:
+        conn = connect_to_db()
 
-    extract_time = datetime.now()
-    extract_year = extract_time.year
-    extract_month = extract_time.month
-    extract_day = extract_time.day
-    extract_hour = extract_time.hour
-    extract_minute = extract_time.minute
+        date = datetime.now()
+        date_str = date.strftime("%Y/%m/%d/%H-%M")
 
-    totesys_table_list = [
-        "address",
-        "design",
-        "transaction",
-        "sales_order",
-        "counterparty",
-        "payment",
-        "staff",
-        "purchase_order",
-        "payment_type",
-        "currency",
-        "department",
-    ]
+        totesys_table_list = [
+            "address",
+            "design",
+            "transaction",
+            "sales_order",
+            "counterparty",
+            "payment",
+            "staff",
+            "purchase_order",
+            "payment_type",
+            "currency",
+            "department",
+        ]
 
-    conn = connect_to_db()
-    print(conn)
+        for table in totesys_table_list:
+            query = f"""SELECT * FROM {table}
+                        LIMIT 2;
+                        """
+            # logic required to check timestamps for updates
 
-    for table in totesys_table_list:
-        query = f"""SELECT * FROM {table}
-                    LIMIT 2 ;
-                    """
-        # logic required to check timestamps for updates
+            response = conn.run(query)
+            columns = [col["name"] for col in conn.columns]
+            formatted_response = {table: format_response(columns, response)}
+            extracted_json = json.dumps(formatted_response, indent=4)
+            # print(extracted_json)
 
-        response = conn.run(query)
-        columns = [col["name"] for col in conn.columns]
-        formatted_response = {table: format_response(columns, response)}
-        extracted_json = json.dumps(formatted_response, indent=4)
-        print(extracted_json)
+            s3_key = f"{table}/{date_str}.json"
 
-        s3_key = f"""{table}/{extract_year}/
-                    {extract_month}/{extract_day}/
-                    {extract_hour}/{extract_minute}.json"""
+            most_recent_extract = s3_key
 
-        most_recent_extract = s3_key
+            s3_client.put_object(
+                Bucket=bucket,
+                Key=s3_key,
+                Body=extracted_json
+            )
 
-        response = bucket.put_object(
-            Key="s3_key",
-            Body=extracted_json,
-        )
+            # response = bucket.put_object(
+            #     Key=s3_key,
+            #     Body=extracted_json,
+            # )
 
-        print(s3_key)
-    conn.close()
-    
-    return extracted_json
+            # print(s3_key)
+        contents = s3_client.list_objects(Bucket=bucket)['Contents']
+        list_of_files = [b["Key"] for b in contents]
+        return list_of_files
+    except ClientError as e:
+        name = __name__
+        log_message(name, "40", e.response["Error"]["Message"])
+    finally:
+        conn.close()
--- test/test_extract.py
+++ test/test_extract.py
@@ -1,7 +1,11 @@
 import pytest
+import json
 from moto import mock_aws
 import boto3
+import os
+from dotenv import load_dotenv
 from src.extract import extract_from_db_write_to_s3
+from pprint import pprint
 from utilities.utils_for_testing import (
     create_s3_bucket,
     upload_to_s3,
@@ -9,6 +13,15 @@ from utilities.utils_for_testing import (
     credentials_storer,
     secret_retriever
 )
+
+
+load_dotenv()
+
+
+@pytest.fixture()
+def s3_client():
+    with mock_aws():
+        yield boto3.client("s3")
 
 
 @pytest.fixture()
@@ -23,24 +36,54 @@ def secretsmanager_client():
         yield boto3.client("secretsmanager")
 
 
-# @pytest.mark.skip()
-def test_get_bucket_contents(s3_resource):
-    bucket = create_s3_bucket("test-bucket", s3_resource=s3_resource)
-    upload_to_s3("test.txt", bucket)
-    upload_to_s3("test2.txt", bucket)
-    assert view_bucket_contents(bucket) == ["test.txt", "test2.txt"]
+@pytest.fixture(scope="function")
+def create_secrets(secretsmanager_client):
+    secret_string={
+        "username": os.getenv("Username"),
+        "password": os.getenv("Password"),
+        "host": os.getenv("Hostname"),
+        "port": os.getenv("Port"),
+        "dbname": os.getenv("Database")
+    }
+    secret=json.dumps(secret_string)
+    secretsmanager_client.create_secret(
+        Name="project-onyx/totesys-db-login", SecretString=secret
+    )
 
 
-def test_extract(s3_resource):
-    bucket = create_s3_bucket("test-bucket", s3_resource=s3_resource)
-    print(bucket)
-    assert extract_from_db_write_to_s3(bucket) == view_bucket_contents(bucket)
-
-
-@pytest.mark.skip()
 def test_get_secret(secretsmanager_client):
     credentials_storer("secret", "bob", "marley", secretsmanager_client)
     assert secret_retriever("secret", secretsmanager_client) == {
         "username": "bob",
         "password": "marley",
     }
+
+
+class TestExtract:
+    def test_extract_writes_to_s3(self, s3_client, create_secrets):
+        s3_client.create_bucket(Bucket="bucket", CreateBucketConfiguration={
+            'LocationConstraint': 'eu-west-2',
+            'Location': {
+                'Type': 'AvailabilityZone',
+                'Name': 'string'
+            }})
+        result = extract_from_db_write_to_s3(s3_client, "bucket")
+        expected_list_bucket = s3_client.list_objects(Bucket="bucket")['Contents']
+        expected = [bucket["Key"] for bucket in expected_list_bucket]
+        print(result)
+        assert result == expected
+    
+    @pytest.mark.skip()
+    def test_initial_extract_entire_db_into_s3(self, s3_client, create_secrets):
+        s3_client.create_bucket(Bucket="bucket", CreateBucketConfiguration={
+            'LocationConstraint': 'eu-west-2',
+            'Location': {
+                'Type': 'AvailabilityZone',
+                'Name': 'string'
+            }})
+        result = extract_from_db_write_to_s3(s3_client, "bucket")
+        expected_list_bucket = s3_client.list_objects(Bucket="bucket")['Contents']
+        expected = [bucket["Key"] for bucket in expected_list_bucket]
+        print(result)
+        assert result == expected
+
