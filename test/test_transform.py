692eae8532cf9c430b23d93afe89b32dbc643c75
ArifJSyed <arif@syeduk.com>
2024-08-16 17:33:08 +0100

    commented out the test_transform file

--- test/test_transform.py
+++ test/test_transform.py
@@ -5,7 +5,7 @@ import boto3
 import os
 from dotenv import load_dotenv
 from src.extract import extract_from_db_write_to_s3
-from src.transform import transform
+#from src.transform import transform
 
 
 @pytest.fixture()
@@ -59,36 +59,36 @@ def write_files_to_ingested_date_bucket(create_secrets, s3_data_buckets):
     extract_from_db_write_to_s3("onyx-totesys-ingested-data-bucket")
 
 
-class TestTransform:
-    @pytest.mark.skip
-    def test_transform_puts_files_in_processed_data_bucket(
-        self, s3_client, write_files_to_ingested_date_bucket
-    ):
-        ingested_data_files = s3_client.list_objects(
-            Bucket="onyx-totesys-ingested-data-bucket"
-        )["Contents"]
-        ingested_files = [bucket["Key"] for bucket in ingested_data_files]
+# class TestTransform:
+#     @pytest.mark.skip
+#     def test_transform_puts_files_in_processed_data_bucket(
+#         self, s3_client, write_files_to_ingested_date_bucket
+#     ):
+#         ingested_data_files = s3_client.list_objects(
+#             Bucket="onyx-totesys-ingested-data-bucket"
+#         )["Contents"]
+#         ingested_files = [bucket["Key"] for bucket in ingested_data_files]
 
-        for file in ingested_files:
-            transform(
-                "onyx-totesys-ingested-data-bucket", file, "onyx-processed-data-bucket"
-            )
+#         for file in ingested_files:
+#             transform(
+#                 "onyx-totesys-ingested-data-bucket", file, "onyx-processed-data-bucket"
+#             )
 
-        result_list_processed_data_bucket = s3_client.list_objects(
-            Bucket="onyx-processed-data-bucket"
-        )["Contents"]
-        result = [bucket["Key"] for bucket in result_list_processed_data_bucket]
-        print(result)
+#         result_list_processed_data_bucket = s3_client.list_objects(
+#             Bucket="onyx-processed-data-bucket"
+#         )["Contents"]
+#         result = [bucket["Key"] for bucket in result_list_processed_data_bucket]
+#         print(result)
 
-        expected = [
-            "dim_staff.parquet",
-            "dim_location.parquet",
-            "dim_design.parquet",
-            "dim_date.parquet",
-            "dim_currency.parquet",
-            "dim_counterparty.parquet",
-            "fact_sales_order.parquet",
-        ]
+#         expected = [
+#             "dim_staff.parquet",
+#             "dim_location.parquet",
+#             "dim_design.parquet",
+#             "dim_date.parquet",
+#             "dim_currency.parquet",
+#             "dim_counterparty.parquet",
+#             "fact_sales_order.parquet",
+#         ]
 
-        for table in expected:
-            assert any([folder.startswith(table) for folder in result])
+#         for table in expected:
+#             assert any([folder.startswith(table) for folder in result])
