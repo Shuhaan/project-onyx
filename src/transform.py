c7006493c0b7af191085bf76101696b09f43f770
Shuhaan <shuhaan15@gmail.com>
2024-08-16 12:17:46 +0100

    dim currency and design added

--- src/transform.py
+++ src/transform.py
@@ -51,3 +51,33 @@ def transform(source_bucket, output_bucket):
             s3_client.upload_file(
                 "dim_location.parquet", output_bucket, "dim_location.parquet"
             )
+        elif table == "design":
+            file = files[0]
+            json_file = s3_client.get_object(Bucket=source_bucket, Key=file)
+            json_contents = json_file["Body"].read().decode("utf-8")
+            data = json.loads(json_contents)["design"]
+
+            df = pd.DataFrame(data)
+            df = df.drop(["created_at", "last_updated"], axis=1)
+            df.to_parquet("dim_design.parquet")
+            s3_client.upload_file(
+                "dim_design.parquet", output_bucket, "dim_design.parquet"
+            )
+        elif table == "currency":
+            file = files[0]
+            json_file = s3_client.get_object(Bucket=source_bucket, Key=file)
+            json_contents = json_file["Body"].read().decode("utf-8")
+            data = json.loads(json_contents)["currency"]
+
+            df = pd.DataFrame(data)
+            df = df.drop(["created_at", "last_updated"], axis=1).assign(
+                currency_name=[
+                    "British Pound Sterling",
+                    "United States Dollar",
+                    "Euros",
+                ]
+            )
+            df.to_parquet("dim_currency.parquet")
+            s3_client.upload_file(
+                "dim_currency.parquet", output_bucket, "dim_currency.parquet"
+            )
