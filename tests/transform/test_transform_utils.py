87b1ad7dccc2cbc5c3b571b82f717d3c58d70e87
Shuhaan <shuhaan15@gmail.com>
2024-08-18 02:13:02 +0100

    move test for log message to sahred utils test file

--- tests/transform/test_transform_utils.py
+++ tests/transform/test_transform_utils.py
@@ -1,33 +1,6 @@
-import pytest, logging
+import pytest
 import pandas as pd
-from extract_lambda.extract import extract
-from transform_lambda.utils import log_message, create_df_from_json_in_bucket
-
-
-class TestLogMessage:
-    @pytest.mark.parametrize(
-        "level, level_name, message",
-        [
-            (10, "DEBUG", "This is a debug message"),
-            (20, "INFO", "This is an info message"),
-            (30, "WARNING", "This is a warning message"),
-            (40, "ERROR", "This is an error message"),
-            (50, "CRITICAL", "This is a critical message"),
-        ],
-        ids=[
-            "Debug level",
-            "Info level",
-            "Warning level",
-            "Error level",
-            "Critical level",
-        ],
-    )
-    def test_log_message_levels(self, caplog, level, level_name, message):
-        caplog.set_level(level)
-        log_message("function_name", level, message)
-
-        assert caplog.messages == [message]
-        assert level_name in caplog.text
+from transform_lambda.utils import create_df_from_json_in_bucket
 
 
 class TestCreateDFFromJSONInBucket:
