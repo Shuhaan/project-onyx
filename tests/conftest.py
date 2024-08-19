153cec657e0d998991dbd946e3876775d1682c26
Shuhaan <shuhaan15@gmail.com>
2024-08-19 11:37:45 +0100

    restructure and download data from actual ingested s3 bucket

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,6 @@
 import pytest, boto3, os, json
 from moto import mock_aws
 from dotenv import load_dotenv
-from unittest.mock import patch
-
-class MockedConnection:
-    def __init__(
-        self, user="user", password="pass", database="db", host="host", port=5432
-    ):
-        self.user = user
-        self.password = password
-        self.database = database
-        self.host = host
-        self.port = port
-        self.columns = [
-            {"name": "data_id"},
-            {"name": "meaningful_data"},
-            {"name": "last_updated"},
-        ]
-        self.rows_data1 = [
-            ["1", "old_data1", "1970-01-01 20:00:00"],
-            ["2", "old_data2", "1970-01-01 20:00:00"],
-        ]
-
-        self.rows_data2 = [
-            ["1", "new_data1", "1980-01-01 20:00:00"],
-            ["2", "old_data2", "1970-01-01 20:00:00"],
-        ]
-
-    def run(self, query):
-        if "WHERE" in query:
-            return self.rows_data2
-        return self.rows_data1
-
-    def close(self):
-        pass
 
 
 # scope="function"
@@ -44,17 +11,6 @@ def aws_credentials():
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
-
-
-@pytest.fixture()
-def mock_db_connection():
-    return MockedConnection()
-
-
-@pytest.fixture()
-def patch_db_connection(mock_db_connection):
-    with patch("extract_lambda.extract.connect_to_db", return_value=MockedConnection()):
-        yield mock_db_connection
 
 
 @pytest.fixture()
