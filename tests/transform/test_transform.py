79f77d6685bbf58136c9ae3b2d3f6181cca414c9
HasanBM <hasanbmalik@gmail.com>
2024-08-19 11:20:41 +0100

    test-ingested-bucket and test-processed-bucket variable names are now consistent

--- tests/transform/test_transform.py
+++ tests/transform/test_transform.py
@@ -3,20 +3,22 @@ from transform_lambda.transform import transform
 
 
 class TestTransform:
-    @pytest.mark.xfail
+    # @pytest.mark.xfail
     def test_transform_puts_files_in_processed_data_bucket(
-        self, s3_client, write_files_to_ingested_data_bucket
+        self, patch_db_connection, write_files_to_ingested_data_bucket
     ):
-        ingested_data_files = s3_client.list_objects(Bucket="test_ingested_bucket")[
-            "Contents"
-        ]
+        ingested_data_files = write_files_to_ingested_data_bucket.list_objects(
+            Bucket="test_ingested_bucket"
+        )["Contents"]
         new_files = [bucket["Key"] for bucket in ingested_data_files]
 
         transform("test_ingested_bucket", new_files, "test_processed_bucket")
 
-        result_list_processed_data_bucket = s3_client.list_objects(
-            Bucket="test_processed_bucket"
-        )["Contents"]
+        result_list_processed_data_bucket = (
+            write_files_to_ingested_data_bucket.list_objects(
+                Bucket="test_processed_bucket"
+            )["Contents"]
+        )
         result = [bucket["Key"] for bucket in result_list_processed_data_bucket]
         print(result)
 
