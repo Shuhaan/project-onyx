13319b1716a1a84436ca7df1a8776c032c71ad8b
Shuhaan <shuhaan15@gmail.com>
2024-08-21 10:26:43 +0100

    remove packages from requiremnts and code for zipping layer, add predefined panda layer

--- terraform/transform/s3.tf
+++ terraform/transform/s3.tf
@@ -1,10 +1,10 @@
-# upload zip file to s3
-resource "aws_s3_object" "layer_code" {
-  bucket     = var.lambda_code_bucket
-  key        = "${var.transform_lambda}/transform_layer.zip"
-  source     = "${path.module}/../../src/transform_lambda/transform_layer.zip"
-  depends_on = [null_resource.lambda_layer] # triggered only if the zip file is created
-}
+# # upload zip file to s3
+# resource "aws_s3_object" "layer_code" {
+#   bucket     = var.lambda_code_bucket
+#   key        = "${var.transform_lambda}/transform_layer.zip"
+#   source     = "${path.module}/../../src/transform_lambda/transform_layer.zip"
+#   depends_on = [null_resource.lambda_layer] # triggered only if the zip file is created
+# }
 
 resource "aws_s3_bucket_notification" "aws-transform-trigger" {
   bucket = var.ingested_data_bucket
