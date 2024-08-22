d5f15e03f3f2c9163c5f729773376904da151108
Saif <admin@saifuddin.uk>
2024-08-22 14:49:24 +0100

    started on load terraform files

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,6 +22,10 @@ resource "aws_iam_role" "extract_lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.trust_policy.json
 }
 
+resource "aws_iam_role" "load_lambda_role" {
+  name_prefix        = "role-${var.load_lambda}"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json
+}
 
 # Create
 resource "aws_iam_role" "transform_lambda_role" {
@@ -46,6 +50,17 @@ data "aws_iam_policy_document" "s3_extract_data_policy_doc" {
   }
 }
 
+data "aws_iam_policy_document" "s3_load_data_policy_doc" {
+  statement {
+    actions = ["s3:PutObject", "s3:ListBucket", "s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.ingested_data_bucket.arn}",
+      "${aws_s3_bucket.ingested_data_bucket.arn}/*"
+    ]
+    effect = "Allow"
+  }
+}
+
 # Create
 resource "aws_iam_policy" "s3_extract_write_policy" {
   name_prefix = "s3-policy-${var.extract_lambda}-read-and-write"
@@ -58,6 +73,16 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_extract_write_policy_attach
   policy_arn = aws_iam_policy.s3_extract_write_policy.arn
 }
 
+resource "aws_iam_policy" "s3_load_write_policy" {
+  name_prefix = "s3-policy-${var.load_lambda}-read-and-write"
+  policy      = data.aws_iam_policy_document.s3_load_data_policy_doc.json
+}
+
+# Attach
+resource "aws_iam_role_policy_attachment" "lambda_s3_load_write_policy_attachment" {
+  role       = aws_iam_role.load_lambda_role.name
+  policy_arn = aws_iam_policy.s3_load_write_policy.arn
+}
 
 # --------------------------------------
 # Extract Lambda IAM Policy for CloudWatch
